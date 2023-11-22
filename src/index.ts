691cffbfbb5f68c848ad4e3fb9018e82729e8718
asep komarudin <pojokcodeid@gmail.com>
2023-11-22 21:58:00 +0700

    update message

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const port: number =
   process.env.PORT != null ? parseInt(process.env.PORT) : 3000
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!')
+  res.send('Hello World 2!')
 })
 
 app.listen(port, () => {
