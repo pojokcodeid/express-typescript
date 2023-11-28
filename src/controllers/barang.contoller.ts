e012d07751db5d493cf3cd1fc330f35d6d16c658
asep komarudin <pojokcodeid@gmail.com>
2023-11-28 16:55:34 +0700

    add CRUD barang

--- src/controllers/barang.contoller.ts
+++ src/controllers/barang.contoller.ts
@@ -1,6 +1,12 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { inputBarangValidation } from '../validations/barang.validation'
-import { getBarang } from '../services/barang.service'
+import {
+  deleteBarang,
+  getBarang,
+  getBarangById,
+  insertBarang,
+  updateBarang
+} from '../services/barang.service'
 
 export const getAllBarang = async (
   req: Request,
@@ -24,11 +30,34 @@ export const getAllBarang = async (
   }
 }
 
-export const insertBarang = (
+export const getDataBarangById = async (
   req: Request,
   res: Response,
   next: NextFunction
-): any => {
+): Promise<any> => {
+  try {
+    const { id } = req.params
+    const barang = await getBarangById(parseInt(id))
+    return res.status(200).json({
+      error: null,
+      message: 'Pengambilan data sukses',
+      data: barang
+    })
+  } catch (error: Error | any) {
+    next(
+      new Error(
+        'Error pada file src/controllers/barang.controller.ts : ' +
+          error.message
+      )
+    )
+  }
+}
+
+export const insertDataBarang = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const { error, value } = inputBarangValidation(req.body)
     if (error != null) {
@@ -38,16 +67,70 @@ export const insertBarang = (
         data: value
       })
     }
-
+    const barang = await insertBarang(value)
     return res.status(200).json({
       error: null,
       message: 'Input data sukses',
-      data: value
+      data: barang
     })
   } catch (error: Error | any) {
     next(
       new Error(
         'Error pada file src/controllers/barang.controller.ts : ' +
+          error.message
+      )
+    )
+  }
+}
+
+export const updateDataBarang = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { id } = req.params
+    const { error, value } = inputBarangValidation(req.body)
+    if (error != null) {
+      return res.status(400).json({
+        error: error.details[0].message,
+        message: 'Input data gagal',
+        data: value
+      })
+    }
+    const data = await updateBarang({ ...value, id: parseInt(id) })
+    return res.status(200).json({
+      error: null,
+      message: 'Update data sukses',
+      data
+    })
+  } catch (error: Error | any) {
+    next(
+      new Error(
+        'Error pada file src/controllers/barang.controller.ts : updateDataBarang' +
+          error.message
+      )
+    )
+  }
+}
+
+export const deleteDataBarang = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { id } = req.params
+    const data = await deleteBarang(parseInt(id))
+    return res.status(200).json({
+      error: null,
+      message: 'Delete data sukses',
+      data
+    })
+  } catch (error: Error | any) {
+    next(
+      new Error(
+        'Error pada file src/controllers/barang.controller.ts : deleteDataBarang' +
           error.message
       )
     )
--- src/routes/barang.route.ts
+++ src/routes/barang.route.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express'
-import { getAllBarang, insertBarang } from '../controllers/barang.contoller'
+import {
+  deleteDataBarang,
+  getAllBarang,
+  getDataBarangById,
+  insertDataBarang,
+  updateDataBarang
+} from '../controllers/barang.contoller'
 import expressAsyncHandler from 'express-async-handler'
 const barangRouter = Router()
 
 barangRouter.get('/barang', expressAsyncHandler(getAllBarang))
-barangRouter.post('/barang', insertBarang)
+barangRouter.get('/barang/:id', expressAsyncHandler(getDataBarangById))
+barangRouter.post('/barang', expressAsyncHandler(insertDataBarang))
+barangRouter.put('/barang/:id', expressAsyncHandler(updateDataBarang))
+barangRouter.delete('/barang/:id', expressAsyncHandler(deleteDataBarang))
 
 export default barangRouter
